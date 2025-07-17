36f505a3696d7db4f111e44fa11db605aac978b8
Sekou Ko <toto.koze@gmail.com>
2025-07-17 22:43:11 +0200

    💡 Ajout détection environnement (localhost vs Heroku)

--- config.php
+++ config.php
@@ -1,9 +1,19 @@
 <?php
-$host = getenv("DB_HOST");
-$port = getenv("DB_PORT");
-$db   = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
+// Vérifie si on est sur Heroku (car ces variables sont définies dans l'environnement Heroku)
+if (getenv("DB_HOST")) {
+    $host = getenv("DB_HOST");
+    $port = getenv("DB_PORT");
+    $db   = getenv("DB_NAME");
+    $user = getenv("DB_USER");
+    $pass = getenv("DB_PASS");
+} else {
+    // Sinon on est en local : paramètres de XAMPP
+    $host = "localhost"; // 
+    $port = "3306";
+    $db   = "zoo_db"; // 🔁 remplace par le vrai nom
+    $user = "root";
+    $pass = ""; // mot de passe vide dans XAMPP
+}
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8";
@@ -13,5 +23,6 @@ try {
     echo "Erreur de connexion à la base de données : " . $e->getMessage();
     exit;
 }
-
 ?>
+
+
